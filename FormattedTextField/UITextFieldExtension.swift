d6535a44df613f9d01c9aa6e996d7eca4698c716
Evgeniy Yurtaev <evgeniyyurt@gmail.com>
2016-10-25 07:51:41 +0300

    Create `UITextField` extension for set a selected range via `Range<String.Index>`

---
+++ FormattedTextField/UITextFieldExtension.swift
@@ -0,0 +1,41 @@
+//
+//  UITextFieldExtension.swift
+//  FormattedTextField
+//
+//  Created by Evgeniy Yurtaev on 25/10/2016.
+//  Copyright © 2016 Evgeniy Yurtaev. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    var selectedCharachtersRange: Range<String.Index>? {
+        get {
+            guard let selectedTextRange = selectedTextRange else {
+                return nil
+            }
+            guard let text = text else {
+                return nil
+            }
+            let location = offset(from: beginningOfDocument, to: selectedTextRange.start)
+            let length = offset(from: selectedTextRange.start, to: selectedTextRange.end)
+            let range = text.range(fromUtf16NsRange: NSMakeRange(location, length))
+
+            return range
+        }
+        set(value) {
+            guard let value = value else {
+                selectedTextRange = nil
+                return
+            }
+            guard let text = text else {
+                return
+            }
+            let utf16Range = text.utf16Nsrange(fromRange: value)
+
+            let from = position(from: beginningOfDocument, offset: utf16Range.location)!
+            let to = position(from: from, offset: utf16Range.length)!
+            selectedTextRange = textRange(from: from, to: to)
+        }
+    }
+}
